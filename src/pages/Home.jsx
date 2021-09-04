80ea88e1d16cfc487acb7046289de585ca3a6daf
arodrigueze0215 <andres.rodriguez0215@gmail.com>
2021-09-04 18:30:12 -0500

    - Fragment small leaf components the ItemDetail page - Using Layout components.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import NavBar from '../components/NavBar'
+import LayoutNavBar from '../components/LayoutNavBar';
 import CardItemList from '../components/CartItemList'
  
 class Home extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <NavBar basketIconActive={false}/>
-                <main>                    
-                    <div className="cardList">
-                        <CardItemList/>
-                        <CardItemList/>
-                        <CardItemList/>
-                        <CardItemList/>
-                        <CardItemList/>
-                    </div>
-                </main>
-            </React.Fragment>
+            <LayoutNavBar>
+                <div className="cardList">
+                    <CardItemList/>
+                    <CardItemList/>
+                    <CardItemList/>
+                    <CardItemList/>
+                    <CardItemList/>
+                </div>
+            </LayoutNavBar>
         )
     }
 }
